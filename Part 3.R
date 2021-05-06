5b091a70aa75c7fb0e7ea2c466ad789a9b376a2c
Pj Fitchett <p.fitchett2@newcastle.ac.uk>
2021-05-06 21:22:17 +0100

    Edited interactive map, added in NBN reptile data and changed some order of code to make more sense.  Added www/ to gitignore

--- Part 3.R
+++ Part 3.R
@@ -7,61 +7,11 @@ options("rgdal_show_exportToProj4_warnings"="none")
 library(sf)
 library(raster)
 
-# Import elevation and shp files ####
+# Import data
 
-# Import elevation data
-elevation <- raster("spatial/elevation.tif")
+# Elevation ####
+elevation <- raster("www/spatial/elevation.tif")
 plot(elevation)
-
-# Import lakes data
-lakes <- st_read("spatial/cumbria_lakes.shp")
-print(lakes)
-# Check coordinate system
-st_crs(lakes)
-# Reset to OS 27700
-lakes <- lakes %>% 
-  st_set_crs(27700) %>% 
-  st_transform(27700)
-# Transform to latitude longitude
-lakes_ll <- st_transform(lakes, 4326) # Lat-Lon
-
-# Import rivers data
-rivers <- st_read("spatial/cumbria_rivers.shp")
-print(rivers)
-# Check coordinate system
-st_crs(rivers)
-# Reset to OS 27700
-rivers <- rivers %>% 
-  st_set_crs(27700) %>% 
-  st_transform(27700)
-# Transform to latitude longitude
-rivers_ll <- st_transform(rivers, 4326) # Lat-Lon
-
-# Import roads data
-roads <- st_read("spatial/cumbria_roads.shp")
-print(roads)
-# Check coordinate system
-st_crs(roads)
-# Reset to OS 27700
-roads <- roads %>% 
-  st_set_crs(27700) %>% 
-  st_transform(27700)
-# Transform to latitude longitude
-roads_ll <- st_transform(roads, 4326) # Lat-Lon
-
-# Import settlements data
-settlements <- st_read("spatial/cumbria_settlements.shp")
-print(settlements)
-# Check coordinate system
-st_crs(settlements)
-# Reset to OS 27700
-settlements <- settlements %>% 
-  st_set_crs(27700) %>% 
-  st_transform(27700)
-# Transform to latitude longitude
-settlements_ll <- st_transform(settlements, 4326) # Lat-Lon
-
-# Working with elevation ####
 
 # Change colours so green = low, brown = high
 plot(elevation, col = terrain.colors(30)) # 30 categories
@@ -80,8 +30,60 @@ plot(elevation, col = terrain.colors(25), alpha = 0.5, add = TRUE)
 
 # Creating contours from the raster DTM
 elevation_contours <- rasterToContour(elevation) %>% st_as_sf()
-plot(elevation)
+plot(elevation, col = terrain.colors(25))
 plot(elevation_contours, add = TRUE)
+# Convert to latlong
+elevation_contours_ll <- st_transform(elevation_contours, 4326)
+
+# Lakes ####
+lakes <- st_read("www/spatial/cumbria_lakes.shp")
+print(lakes)
+# Check coordinate system
+st_crs(lakes)
+# Reset to OS 27700
+lakes <- lakes %>% 
+  st_set_crs(27700) %>% 
+  st_transform(27700)
+# Transform to latitude longitude
+lakes_ll <- st_transform(lakes, 4326) # Lat-Lon
+
+# Rivers ####
+rivers <- st_read("www/spatial/cumbria_rivers.shp")
+print(rivers)
+# Check coordinate system
+st_crs(rivers)
+# Reset to OS 27700
+rivers <- rivers %>% 
+  st_set_crs(27700) %>% 
+  st_transform(27700)
+# Transform to latitude longitude
+rivers_ll <- st_transform(rivers, 4326) # Lat-Lon
+
+# Roads ####
+roads <- st_read("www/spatial/cumbria_roads.shp")
+print(roads)
+# Check coordinate system
+st_crs(roads)
+# Reset to OS 27700
+roads <- roads %>% 
+  st_set_crs(27700) %>% 
+  st_transform(27700)
+# Transform to latitude longitude
+roads_ll <- st_transform(roads, 4326) # Lat-Lon
+
+# Settlements ####
+settlements <- st_read("www/spatial/cumbria_settlements.shp")
+print(settlements)
+# Check coordinate system
+st_crs(settlements)
+# Reset to OS 27700
+settlements <- settlements %>% 
+  st_set_crs(27700) %>% 
+  st_transform(27700)
+# Transform to latitude longitude
+settlements_ll <- st_transform(settlements, 4326) # Lat-Lon
+
+# Maps with multiple features ####
 
 # Plot elevation and overlay features - eg roads, lakes, rivers
 plot(elevation)
@@ -90,18 +92,124 @@ plot(rivers, add = TRUE)
 plot(roads, add = TRUE)
 plot(settlements, add = TRUE)
 
-# Make the map interactive - optional layers
+# Make the map interactive with optional layers
 leaflet() %>% 
   addTiles(group = "OSM (default)") %>% 
   addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>% 
-  addFeatures(lakes_ll, group = "Lakes") %>% 
-  addFeatures(rivers_ll, group = "Rivers") %>%
-  addFeatures(roads_ll, group = "Roads") %>%
-  addFeatures(settlements_ll, group = "Settlements") %>%
+# stroke = TRUE, weight = 1 gives an outline and fills in shapes with a lighter colour
+  addFeatures(lakes_ll, group = "Lakes",
+              stroke = TRUE, weight = 1) %>% 
+  addFeatures(rivers_ll, group = "Rivers",
+              stroke = TRUE, weight = 1) %>%
+  addFeatures(roads_ll, group = "Roads",
+              stroke = TRUE, weight = 1, color = "red") %>%
+  addFeatures(settlements_ll, group = "Urban areas",
+              stroke = TRUE, weight = 1, color = "black") %>%
+  addRasterImage(elevation_ll, col = terrain.colors(25), 
+                 opacity = 0.6, group = "Elevation") %>%
+  # Hide groups so they don't automatically show
+  hideGroup(c("Lakes", "Rivers", "Roads", "Urban areas", "Elevation")) %>%
+  # Allow the user to choose which layers they want to see
   addLayersControl(
     baseGroups = c("OSM (default)", "Satellite"), 
-    overlayGroups = c("Lakes", "Rivers", "Roads", "Settlements"),
+    overlayGroups = c("Lakes", "Rivers", "Roads", "Urban areas", "Elevation"),
     options = layersControlOptions(collapsed = FALSE)
   )
+
+
+
+
+
+# Citizen science data from NBN ####
+
+# Common lizard ####
+common <- read.csv("www/common_lizard_cumbria/common_lizard_cumbria.csv")
+common <- common[common$identificationVerificationStatus.processed == "Accepted",]
+# Records per year for UK
+records_per_yr_common <- common %>%
+  group_by(year.processed) %>%
+  summarise(count_per_year = n())
+ggplot(records_per_yr_common, aes(x = year.processed, y=count_per_year)) +
+  geom_line() +
+  ggtitle("Common Lizard") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Year") +
+  ylab("Number of records")
+# Create a map showing records of common lizard
+common_map <- addProviderTiles(leaflet(),"Esri.WorldImagery") %>%
+  addCircleMarkers(common$decimalLongitude.processed, common$decimalLatitude.processed,  
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="red") %>%
+  addLegend(colors = "red", opacity=1, labels="Common Lizard")
+common_map
+
+
+# Adder ####
+adder <- read.csv("www/adder_cumbria/adder_cumbria.csv")
+adder <- adder[adder$identificationVerificationStatus.processed == "Accepted",]
+# Records per year for UK
+records_per_yr_adder <- adder %>%
+  group_by(year.processed) %>%
+  summarise(count_per_year = n())
+ggplot(records_per_yr_adder, aes(x = year.processed, y=count_per_year)) +
+  geom_line() +
+  ggtitle("Adder") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Year") +
+  ylab("Number of records")
+# Create a map showing records of adder
+adder_map <- addProviderTiles(leaflet(),"Esri.WorldImagery") %>%
+  addCircleMarkers(adder$decimalLongitude.processed, adder$decimalLatitude.processed,  
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="red") %>%
+  addLegend(colors = "red", opacity=1, labels="Adder")
+adder_map
+
+
+# Slow worm ####
+slow <- read.csv("www/slow_worm_cumbria/slow_worm_cumbria.csv")
+slow <- slow[slow$identificationVerificationStatus.processed == "Accepted",]
+# Records per year for UK
+records_per_yr_slow <- slow %>%
+  group_by(year.processed) %>%
+  summarise(count_per_year = n())
+ggplot(records_per_yr_slow, aes(x = year.processed, y=count_per_year)) +
+  geom_line() +
+  ggtitle("Slow Worm") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Year") +
+  ylab("Number of records")
+# Create a map showing records of slow worm
+slow_map <- addProviderTiles(leaflet(),"Esri.WorldImagery") %>%
+  addCircleMarkers(slow$decimalLongitude.processed, slow$decimalLatitude.processed,  
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="red") %>%
+  addLegend(colors = "red", opacity=1, labels="Slow Worm")
+slow_map
+
+
+# Grass snake ####
+grass <- read.csv("www/grass_snake_cumbria/grass_snake_cumbria.csv")
+grass <- grass[grass$identificationVerificationStatus.processed == "Accepted",]
+# Records per year for UK
+records_per_yr_grass <- grass %>%
+  group_by(year.processed) %>%
+  summarise(count_per_year = n())
+ggplot(records_per_yr_grass, aes(x = year.processed, y=count_per_year)) +
+  geom_line() +
+  ggtitle("Grass Snake") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Year") +
+  ylab("Number of records")
+# Create a map showing records of grass snake
+grass_map <- addProviderTiles(leaflet(),"Esri.WorldImagery") %>%
+  addCircleMarkers(grass$decimalLongitude.processed, grass$decimalLatitude.processed,  
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="red") %>%
+  addLegend(colors = "red", opacity=1, labels="Grass Snake")
+grass_map
+
+
+
+
+
+
+
 
 
