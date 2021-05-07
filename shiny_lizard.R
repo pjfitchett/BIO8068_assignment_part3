7ef7dd0c44f87a94b2782f704a73a1ebbf591bd3
Pj Fitchett <p.fitchett2@newcastle.ac.uk>
2021-05-07 19:05:32 +0100

    Added images to each tab, aligned to right hand side to allow for text/figures etc

--- shiny_lizard.R
+++ shiny_lizard.R
@@ -1,6 +1,22 @@
-# Shiny app
+# Shiny reptiles app
 
 library(shiny)
+library(leaflet)
+library(leafem)
+library(mapview)
+options("rgdal_show_exportToProj4_warnings"="none")
+library(sf)
+library(raster)
+library(ggplot2)
+library(dplyr)
+
+# Import all relevant data ----
+
+# Add reptile images
+adder_image <- base64enc::dataURI(file="www/adder_image.jpeg", mime="image/jpeg")
+common_image <- base64enc::dataURI(file="www/common_lizard_image.jpeg", mime="image/jpeg")
+grass_image <- base64enc::dataURI(file="www/grass_snake_image.jpeg", mime="image/jpeg")
+slow_image <- base64enc::dataURI(file="www/slow_worm_image.jpeg", mime="image/jpeg")
 
 # Define UI ----
 
@@ -14,13 +30,18 @@ ui <- fluidPage(
         id = "tabset",
         tabPanel("Overview" 
                  ),
-        tabPanel("Adder"
+        tabPanel("Adder",
+                 img(src=adder_image, height="50%", width="50%", align="right")
                  ),
-        tabPanel("Common Lizard"
+        tabPanel("Common Lizard",
+                 img(src=common_image, height="50%", width="50%", align="right")
                  ),
-        tabPanel("Grass Snake"
+        tabPanel("Grass Snake",
+                 img(src=grass_image, height="50%", width="50%", align="right")
                  ),
-        tabPanel("Slow Worm")
+        tabPanel("Slow Worm",
+                 img(src=slow_image, height="50%", width="50%", align="right")
+                 )
       )
     )
   )
